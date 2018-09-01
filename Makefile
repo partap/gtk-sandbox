4af52bb7fe23fcbd7c4c0928b099a7dbf351735e
Partap Davis <partapdavis@gmail.com>
2018-09-01 16:35:12 -0600

    Fixed example 1

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc `pkg-config --cflags gtk+-3.0` -o example-1 example-1.c `pkg-config --libs gtk+-3.0`
+	gcc -Wall `pkg-config --cflags gtk+-3.0` -o example-1 example-1.c `pkg-config --libs gtk+-3.0`
--- example-1.c
+++ example-1.c
@@ -4,7 +4,7 @@ static void
 print_hello (GtkApplication *app,
 		  gpointer data)
 {
-	g_print ("Hello World");
+	g_print ("Hello World\n");
 }
 
 static void
@@ -19,7 +19,10 @@ activate (GtkApplication *app,
 	gtk_window_set_title (GTK_WINDOW (window), "Window");
 	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
-	button_box = gtk_button_new_with_label ("Hello World");
+	button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+	gtk_container_add (GTK_CONTAINER (window), button_box);
+
+	button = gtk_button_new_with_label ("Hello World");
 	g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
 	g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
 	gtk_container_add (GTK_CONTAINER (button_box), button);
